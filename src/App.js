5602a55acb52ee94ca4dc57e86fe3cade046b2c6
Raphael velasco <raphaelvelasco2@gmail.com>
2022-07-20 12:30:34 -0300

    Fiz o Desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+const Div = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-image: url("https://t4.ftcdn.net/jpg/02/59/42/91/360_F_259429114_zRivV3HHiutqocwL8f2rL3MQ3LIXTX5I.jpg");
+`;
+const Img = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: 45px;
+  padding: 20px;
+`;
+const Button = styled.img`
+
+`
+
+export default function DogAPI() {
+  const [dog, setDog] = useState([]);
+  const [imagemAparecendo, setImagemAparecendo] = useState(false);
+
+  function DogAPI() {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      console.log(response);
+      setDog(response.data.message);
+      setImagemAparecendo(true);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <h1 style={{ color:"red" }}>DogAPI</h1>
+      {imagemAparecendo && (
+        <Img src={dog} alt="imagem de doguinho" />
+      )}
+      <button
+        onClick={() => {
+          DogAPI();
+        }}
+      >
+        Clica Aqui
+      </button>
+    </Div>
   );
 }
-
-export default App;
